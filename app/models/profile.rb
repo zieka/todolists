0ebcab5c5adafe1088d3d0f0c04d58c9963b4c47
Kyle Scully <scullykns@gmail.com>
2015-11-27 17:07:51 -0800

    eq11 passed

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,14 @@
 class Profile < ActiveRecord::Base
-    belongs_to :user
+  belongs_to :user
+  validate :name_nil
+
+  def name_nil
+    if !first_name & !last_name
+      errors.add(:last_name, "First and last name cannot both be null")
+    end
+  end
+
+  validates_inclusion_of :gender, :in => ["male", "female"]
+  validates :first_name, exclusion: ["Sue"], if: "gender == 'male'"
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
    has_one :profile, dependent: :destroy
    has_many :todo_lists, dependent: :destroy
    has_many :todo_items, through: :todo_lists, source: :todo_items
+   validates_presence_of :username
 end
