12e54464e1aa9124a96b9da752ebf281d24d6d7b
Kyle Scully <scullykns@gmail.com>
2015-11-27 17:51:15 -0800

    rq13 passed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
-   has_one :profile, dependent: :destroy
-   has_many :todo_lists, dependent: :destroy
-   has_many :todo_items, through: :todo_lists, source: :todo_items
-   validates_presence_of :username
+  has_one :profile, dependent: :destroy
+  has_many :todo_lists, dependent: :destroy
+  has_many :todo_items, through: :todo_lists, source: :todo_items
+  validates_presence_of :username
+
+  def get_completed_count
+    self.todo_items.where(completed: true).count
+  end
 end
