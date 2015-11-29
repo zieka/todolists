2efd04996537d04247e7a711b273a31e0bfe63da
Kyle Scully <scullykns@gmail.com>
2015-11-29 09:51:14 -0800

    added redirect to index after create/update; removed edit from index view

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -28,7 +28,7 @@ class TodoItemsController < ApplicationController
 
     respond_to do |format|
       if @todo_item.save
-        format.html { redirect_to @todo_item, notice: 'Todo item was successfully created.' }
+        format.html { redirect_to todo_items_url, notice: 'Todo item was successfully created.' }
         format.json { render :show, status: :created, location: @todo_item }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class TodoItemsController < ApplicationController
   def update
     respond_to do |format|
       if @todo_item.update(todo_item_params)
-        format.html { redirect_to @todo_item, notice: 'Todo item was successfully updated.' }
+        format.html { redirect_to todo_items_url, notice: 'Todo item was successfully updated.' }
         format.json { render :show, status: :ok, location: @todo_item }
       else
         format.html { render :edit }
