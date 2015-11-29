a1584a7136c81f425d68a6578a82e46825349b27
Kyle Scully <scullykns@gmail.com>
2015-11-29 13:44:08 -0800

    initial setup for assignment 4

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
@@ -45,7 +45,9 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-group :test do
- gem 'rspec-rails', '~> 3.0'
-end
+gem 'will_paginate', '~> 3.0.6'
 
+group :test do
+  gem 'rspec-rails', '~> 3.0'
+  gem 'capybara'
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,44 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+today = Date.today
+two_days_ago = Date.today - 2.days
+three_days_ago = Date.today - 3.days
+dates = [today, two_days_ago, three_days_ago]
 
 User.destroy_all
 TodoList.destroy_all
-TodoItem.destroy_all
-Profile.destroy_all
 
-User.create! [
-  {username: "Fiorina", password_digest: "placeholder"},
-  {username: "Trump", password_digest: "placeholder"},
-  {username: "Carson", password_digest: "placeholder"},
-  {username: "Clinton", password_digest: "placeholder"}
+100.times { |index| TodoList.create! list_name: "List #{index}", list_due_date: dates.sample }
+
+TodoList.all.each do |list|
+  list.todo_items.create! [
+    { title: "Task 1", due_date: dates.sample, description: "very important task TEST", completed: false },
+    { title: "Task 2", due_date: dates.sample, description: "do something else TEST", completed: true},
+    { title: "Task 3", due_date: dates.sample, description: "learn Action Pack TEST", completed: true}
+  ]
+end
+
+users = User.create! [
+  { username: "jim", password: "abc123" },
+  { username: "rich", password: "123abc" }
 ]
 
-Profile.create! [
-  {first_name: "Carly", last_name: "Fiorina", birth_year: 1954, gender: "female"},
-  {first_name: "Donald", last_name: "Trump", birth_year: 1946, gender: "male"},
-  {first_name: "Ben", last_name: "Carson", birth_year: 1951, gender: "male"},
-  {first_name: "Hillary", last_name: "Clinton", birth_year: 1947, gender: "female"}
-]
-
-users = User.all
-due_date = Date.today + 1.year
-
-users.each do |e|
-  e.todo_lists.create!(
-    list_name: "#{e.username}'s list",
-    list_due_date: due_date
-  )
-
-  5.times do
-    e.todo_lists.first.todo_items.create!(
-      due_date: due_date,
-      title: "TITLE",
-      description: "DESC"
-    )
-  end
+TodoList.all.each do |list|
+  list.user = users.sample
+  list.save!
 end
