eb152bc66a63409f3a23f7bacd9c050a4eace5b8
Kyle Scully <scullykns@gmail.com>
2015-11-29 08:52:42 -0800

    initial setup for assignment 3; new specs; new seed; new gem

--- Gemfile
+++ Gemfile
@@ -42,10 +42,11 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 group :test do
- gem 'rspec-rails', '~> 3.0'
+  gem 'rspec-rails', '~> 3.0'
+  gem 'capybara'
 end
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,44 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+today = Date.today
+two_days_ago = Date.today - 2.days
+three_days_ago = Date.today - 3.days
 
-User.destroy_all
-TodoList.destroy_all
 TodoItem.destroy_all
-Profile.destroy_all
 
-User.create! [
-  {username: "Fiorina", password_digest: "placeholder"},
-  {username: "Trump", password_digest: "placeholder"},
-  {username: "Carson", password_digest: "placeholder"},
-  {username: "Clinton", password_digest: "placeholder"}
+TodoItem.create! [
+  { title: "Task 1", due_date: today, description: "very important task TEST", completed: false },
+  { title: "Task 2", due_date: two_days_ago, description: "learn ruby TEST", completed: true},
+  { title: "Task 3", due_date: three_days_ago, description: "learn Active Record TEST", completed: true}
 ]
-
-Profile.create! [
-  {first_name: "Carly", last_name: "Fiorina", birth_year: 1954, gender: "female"},
-  {first_name: "Donald", last_name: "Trump", birth_year: 1946, gender: "male"},
-  {first_name: "Ben", last_name: "Carson", birth_year: 1951, gender: "male"},
-  {first_name: "Hillary", last_name: "Clinton", birth_year: 1947, gender: "female"}
-]
-
-users = User.all
-due_date = Date.today + 1.year
-
-users.each do |e|
-  e.todo_lists.create!(
-    list_name: "#{e.username}'s list",
-    list_due_date: due_date
-  )
-
-  5.times do
-    e.todo_lists.first.todo_items.create!(
-      due_date: due_date,
-      title: "TITLE",
-      description: "DESC"
-    )
-  end
-end
