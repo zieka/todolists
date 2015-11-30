d6147f763e55360bb0e06f432a3a003bdf4cc125
Kyle Scully <scullykns@gmail.com>
2015-11-29 16:12:16 -0800

    generated sessions controlled; modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
+
   root 'todo_lists#index'
 
   resources :todo_lists do
