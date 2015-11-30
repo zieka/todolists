27e823bca50e58619e8def1350f88f3002998c27
Kyle Scully <scullykns@gmail.com>
2015-11-29 16:24:47 -0800

    session controller authenticates login; part 10 complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(username: params[:user][:username])
+
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Logged in successfully"
+    else
+      redirect_to login_path, alert: "Login Failed"
+    end
   end
 
   def destroy
+    reset_session
+    redirect_to login_path, notice: "Logged Out of Session"
   end
 end
